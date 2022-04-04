c07e5dd6d8491c975a0f34ab8ef3350158bdbe19
Robert Butler <robertb@Administrators-MacBook-Pro.local>
2022-04-04 10:25:36 -0700

    I think this should be all the code needed for the data update

--- R/arc_dat_proc.R
+++ R/arc_dat_proc.R
@@ -36,7 +36,7 @@ habitat2022 <- read.csv('data/HabitatDataJSSH2022.csv')
 segment2022 <- read.csv('data/SegmentDataJSSH2022.csv')
 
 
-
+# ----------------------------------------------------------- fishdat ---------------------------------------------------------------
 # Trying to match the naming that Marcus used in the original dat_proc script
 fishdat <- siteannual2022 %>%
   # Below modification to the SiteAnnualData is based on what i see in dat_proc
@@ -53,16 +53,17 @@ fishdat <- siteannual2022 %>%
     SampleDate = as.Date(gsub('00:00:00$', '', SampleDate)),
     Year = ifelse(is.na(Year), year(SampleDate), Year)
   ) %>% 
-  dplyr::select(-SiteYearID, -Dry_rating, -DensTtl, -Dens_A1, -Dens_A2, -YearLabel, -GlobalID, -Sp_Dwarf_SP, -Sp_CbznScp, -SiteSort) 
-  #%>%
-  #st_as_sf(coords=c('X','Y'))
+  dplyr::select(-SiteYearID, -Dry_rating, -DensTtl, -Dens_A1, -Dens_A2, -YearLabel, -GlobalID, -Sp_Dwarf_SP, -Sp_CbznScp, -SiteSort) %>%
+  st_as_sf(coords=c('X','Y'))
 
+
+# --------------------------------------------------------- trndst_prep ---------------------------------------------------------------
 trndst_prep <- fishdat %>% 
   filter(Year > 1981) %>% 
   filter(!(Year == 1994 & Watershed %in% c('PAJ', 'SOQ')))
 
 # separate crds tibble for later joining
-crds <- fishdat %>% dplyr::select('X','Y','SiteID') %>% distinct() 
+crds <- fishdat %>% dplyr::select('geometry','SiteID') %>% distinct() 
 
 # get trends by station, size class
 trndst_prep <- trndst_prep %>% 
@@ -73,18 +74,20 @@ trndst_prep <- trndst_prep %>%
   nest %>% 
   left_join(crds, by = 'SiteID')
 
+save(trndst_prep, file = 'data/trndst_prep.RData', compress = 'xz')
 
 
-habnew <- habitat2022 %>% 
+# ---------------------------------------------------------- habitat aka habnew ---------------------------------------------------------------
+habitat <- habitat2022 %>% 
   dplyr::select(-SiteYearID, -StnNumStrt, -StnNumEnd, -HabDetail, -AssessDate, -YearLabel, -GlobalID, -OBJECTID) %>%
   left_join(
     fishdat_new %>% dplyr::select(SiteID, Year, X, Y, Watershed) %>% distinct, 
     by = c('SiteID','Year')
   )
 
-habnew <- habnew %>% 
+habitat <- habitat %>% 
   pivot_longer(
-    as.vector( names(habnew)[ which( !names(habnew) %in% c('SiteID', 'Year', 'X', 'Y', 'HabType', 'Watershed') ) ] ), 
+    as.vector( names(habitat)[ which( !names(habitat) %in% c('SiteID', 'Year', 'X', 'Y', 'HabType', 'Watershed') ) ] ), 
     names_to = 'habvar', 
     values_to = 'habval'
   ) %>%
@@ -92,15 +95,128 @@ habnew <- habnew %>%
   filter(!is.na(Y)) %>%
   st_as_sf(coords=c('X','Y'))
 
-data(fishdat)
-data(habitat)
+save(habitat, file = 'data/habitat.RData')
+
+# ------------------------------------------------------------------ trndhab_prep ------------------------------------------------------------
+
+# prep habitat site data for trends
 
 
+# prep habitat data, long format 
+trndhab_prep <- habitat
 
+# separate crds tibble for later joing
+# separate crds tibble for later joining
+crds <- fishdat %>% dplyr::select('geometry','SiteID') %>% distinct() 
 
+# get trends by station, size class
+trndhab_prep <- trndhab_prep %>% 
+  dplyr::select(-Watershed) %>% 
+  filter(HabType %in% c('run', 'riffle', 'pool')) %>% 
+  group_by(SiteID, HabType, habvar) %>% 
+  nest %>% 
+  left_join(crds, by = 'SiteID')
 
+save(trndhab_prep, file = 'data/trndhab_prep.RData', compress = 'xz')
 
+# ----------------------------------------------------------- allfctprs -----------------------------------------------------------------------
 
+# combined salmonid, habitat data, only where years intersect
+saldat <- fishdat
+#st_geometry(saldat) <- NULL
+saldat <- saldat %>% 
+  dplyr::select(Year, SiteID, Watershed, Dens_S1, Dens_S2) %>% 
+  group_by(Year, SiteID, Watershed) %>% 
+  summarise(
+    Dens_S1 = mean(Dens_S1, na.rm = T), 
+    Dens_S2 = mean(Dens_S2, na.rm = T)
+  ) %>% 
+  ungroup %>% 
+  mutate(SiteID = as.character(SiteID))
+
+habdat <- habitat
+st_geometry(habdat) <- NULL
+habdat <- habdat %>% 
+  dplyr::select(-Watershed) %>% 
+  group_by(Year, SiteID, HabType) %>% 
+  filter(!habvar %in% 'StnSthd') %>% 
+  nest %>% 
+  filter(HabType %in% c('pool', 'riffle', 'run'))
+
+# combind
+dat <- inner_join(saldat, habdat, by = c('Year', 'SiteID'))
+
+# habitat variables to select, master obj
+habvrs <- list(
+  'Canopy cover (%)' = 'StnCan',
+  'Deciduous canopy cover (%)' = 'StnDecid', 
+  'Average depth (ft)' = 'StnDpthAvg',
+  'Maximum depth (ft)' = 'StnDpthMax',
+  'Embeddedness (%)' = 'StnEmbed',
+  'Escape cover (ratio)' = 'StnEsCov',
+  'Fines (%)' = 'StnFines',
+  'Station length (ft)' = 'StnLgth',
+  'Station width (ft)' = 'StnWdth'
+)
+
+# get the object
+allfctprs <- dat %>% 
+  unnest(data) %>% 
+  group_by(Year, Watershed, HabType, habvar) %>% 
+  summarise(
+    Dens_S1 = mean(Dens_S1, na.rm = T), 
+    Dens_S2 = mean(Dens_S2, na.rm = T), 
+    habval = mean(habval, na.rm = T)
+  ) %>% 
+  gather('densvar', 'densval', Dens_S1, Dens_S2) %>% 
+  group_by(Watershed, HabType, densvar, habvar) %>% 
+  nest %>% 
+  mutate(
+    modsel = pmap(list(densvar, habvar, data), function(densvar, habvar, data){
+      
+      # format data with names
+      names(data)[names(data) %in% 'habval'] <- habvar
+      names(data)[names(data) %in% 'densval'] <- densvar
+      data <- na.omit(data)
+      
+      # formula
+      frm <- paste0('log10(1 + ', densvar, ') ~ Year +', habvar) %>%
+        formula
+      
+      # global
+      modoutglo <- lm(frm, data = data, na.action = na.pass)
+      
+      # selected, summary
+      modoutsel <-  modoutglo %>%
+        dredge %>%
+        get.models(subset = 1) %>%
+        .[[1]]
+      
+      return(modoutsel)
+      
+    }), 
+    modcof = map(modsel, coefficients),
+    modimp = map(modsel, function(modsel){
+      
+      cof <- modsel %>% 
+        summary %>% 
+        coefficients %>% 
+        rownames
+      
+      out <- F
+      if(any(!cof %in% c('(Intercept)', 'Year'))) out <- T
+      
+      return(out)
+      
+    })
+    
+  ) %>% 
+  unnest(modimp) %>% 
+  filter(modimp) %>% 
+  dplyr::select(-data, -habvar, -modcof, -modimp) %>% 
+  arrange(densvar, Watershed, HabType)
+
+save(allfctprs, file = 'data/allfctprs.RData', compress = 'xz')
 
 
 
